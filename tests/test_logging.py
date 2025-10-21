f40c59744a0c0c05afcf4e3a33f3de8e45d49d20
ALN <119984311+alanprawira@users.noreply.github.com>
2025-10-21 21:07:44 +0300

    Fix test

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,14 +1,9 @@
 from fastapi.testclient import TestClient
 from app.main import app
-import os
 
 client = TestClient(app, raise_server_exceptions=False)
 
 def test_logging_hide_password():
-    
-    if os.path.exists('app.log'):
-        os.remove('app.log')
-    
     payload = {"username": "usr", "password": "pwd"}
     resp = client.post("/login_w_error", json=payload)
     print(resp.status_code)
@@ -16,5 +11,4 @@ def test_logging_hide_password():
 
     with open('app.log', 'r') as f:
         file = f.read()
-        # Optional: Tambahkan assertion untuk test
-        assert "pwd" not in file  # Pastikan password tidak kelihatan
+        assert "pwd" not in file
